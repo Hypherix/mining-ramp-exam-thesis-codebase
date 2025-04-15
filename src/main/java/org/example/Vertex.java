e37343d57092e1714898d4f1b82637641cb33931
Hypherix <118616820+Hypherix@users.noreply.github.com>
2025-04-15 14:31:14 +0200

    Undid previous commit

--- src/main/java/org/example/Vertex.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Vertex {
-
-    // Data members
-    private int id;     // needed?
-    private Vertex occupiedBy;  // null per default
-    private Vertex passBay;     // null per default
-}
