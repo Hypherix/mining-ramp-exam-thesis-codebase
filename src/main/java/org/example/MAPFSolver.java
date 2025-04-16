8e013848b602f9b537b53fb29918a501eba39ae1
Hypherix <118616820+Hypherix@users.noreply.github.com>
2025-04-16 12:16:36 +0200

    Added solve method to MAPFSolver class

--- src/main/java/org/example/MAPFSolver.java
+++ src/main/java/org/example/MAPFSolver.java
@@ -1,26 +1,37 @@
 package org.example;
 
+import org.example.algorithms.AlgorithmFactory;
+import org.example.algorithms.MAPFAlgorithm;
+
 
 /*
-* Activates an algorithm to solve a given MAPFScenario
-* Contents:
-*   - MAPFScenario
-*   - Algorithm to use
-*
-* */
+ * Activates an algorithm to solve a given MAPFScenario
+ * Contents:
+ *   - MAPFScenario
+ *   - Algorithm to use
+ *
+ * */
+
 
 public class MAPFSolver {
 
     // Data members
     MAPFScenario scenario;
-    Algorithm algorithm;
+    MAPFAlgorithm algorithm;
 
     // Constructors
     public MAPFSolver(MAPFScenario scenario, String algorithm) {
         this.scenario = scenario;
-        
+        // Invoke the factory pattern to fetch the correct algorithm object
+        this.algorithm = AlgorithmFactory.getAlgorithm(algorithm);
     }
 
     // Methods
+    public void solve() {
+        // Task: Prompts the algorithm to solve the MAPF scenario
+        // As of now, the solve methods return void. When A* is implemented,
+        // return a representation of a solution
 
+        this.algorithm.solve(this.scenario);
+    }
 }
