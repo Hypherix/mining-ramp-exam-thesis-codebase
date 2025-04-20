31f0bc7a98c2915fa1032bbc1742a2b80cfc75cf
Hypherix <118616820+Hypherix@users.noreply.github.com>
2025-04-20 11:02:19 +0200

    Corrected StateComparator class
    Lowest cost now has highest priority.
    
    Added note for tie-breaking

--- src/main/java/org/example/StateComparator.java
+++ src/main/java/org/example/StateComparator.java
@@ -3,16 +3,19 @@ import java.util.Comparator;
 
 /*
 * Enables comparison between MAPFStates based on their costs.
-* Needed for implementation of PriorityQueues
+* Needed for implementation of PriorityQueues.
+* Note! Equal cost states can be polled in any order, regardless of when they
+* were enqueued. If tie-breaker is needed, create a counter data member for the
+* MAPFState that keeps track of when it was enqueued, and bake it in to the comparator
 * */
 
 public class StateComparator implements Comparator<MAPFState> {
     public int compare(MAPFState s1, MAPFState s2) {
         if(s1.getCost() < s2.getCost()) {
-            return 1;
+            return -1;
         }
         else if (s1.getCost() > s2.getCost()) {
-            return -1;
+            return 1;
         }
         return 0;
     }
