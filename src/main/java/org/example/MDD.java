fecd146089bd1f07ac584e659cc391563d96df6a
Hypherix <118616820+Hypherix@users.noreply.github.com>
2025-04-28 21:14:17 +0200

    Undid printMDD() progress
    Not needed to print

--- src/main/java/org/example/MDD.java
+++ src/main/java/org/example/MDD.java
@@ -11,22 +11,5 @@ public class MDD {
     }
 
     // Methods
-    public void printNode(MDDNode node) {
 
-    }
-
-    public void printMDD() {
-        // Task: Print the MD
-
-        MDDNode currentNode = this.root;
-        while(currentNode != null) {
-            System.out.print("[" + currentNode.vertex + "]: " + currentNode.children + ", ");
-
-            for(MDDNode child : currentNode.children) {
-                // TODO: Create a method that prints the node and its children.
-                //  Have the method under MDD or MDDNode(perhaps better in node?)
-                //  Then call that method here
-            }
-        }
-    }
 }
